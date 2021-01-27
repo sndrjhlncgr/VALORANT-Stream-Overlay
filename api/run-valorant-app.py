0d77f7b1c157ca41bf12ea913d460d83342fb178
Sandro Cagara <sandrocagaragithub@gmail.com>
2021-01-27 21:24:38 +0800

    updating code adding helpers and etc

--- api/run-valorant-app.py
+++ api/run-valorant-app.py
@@ -1,20 +1,24 @@
+import os
+import socket
 import urllib
 
-from flask import Flask, Response, render_template, make_response, request
-from dotenv import load_dotenv, find_dotenv
-import os, socket
 import requests
+from dotenv import load_dotenv, find_dotenv
+from flask import Flask, Response, render_template, make_response
 
 hostname = socket.gethostname()
-
 load_dotenv(find_dotenv())
 
-CLIENT_IP = socket.gethostbyname(hostname)
 AUTHORIZATION = 'https://auth.riotgames.com/api/v1/authorization'
+ENTITLEMENT_TOKEN_LINK = 'https://entitlements.auth.riotgames.com/api/token/v1'
+AUTHORIZATION_INFORMATION = 'https://auth.riotgames.com/userinfo'
+
+CLIENT_PLATFORM = 'ew0KCSJwbGF0Zm9ybVR5cGUiOiAiUEMiLA0KCSJwbGF0Zm9ybU9TIjogIldpbmRvd3MiLA0KCSJwbGF0Zm9ybU9TVmVyc2lvbiI6ICIxMC4wLjE5MDQyLjEuMjU2LjY0Yml0IiwNCgkicGxhdGZvcm1DaGlwc2V0IjogIlVua25vd24iDQp9'
 
 RIOT_USERNAME = os.getenv("RIOT_USERNAME")
 RIOT_PASSWORD = os.getenv("RIOT_PASSWORD")
 USER_REGION = os.getenv("USER_REGION")
+CLIENT_IP = socket.gethostbyname(hostname)
 
 app = Flask(__name__, template_folder="components")
 
@@ -33,11 +37,13 @@ def getCookies():
     }
     response = requests.post(AUTHORIZATION, headers=headers, json=credentials)
     cookies = response.cookies
+
     return cookies
 
 
 def getAccessToken():
-    cookies = getCookies()
+    COOKIES = getCookies()
+
     credentials = {
         'type': 'auth',
         'username': RIOT_USERNAME,
@@ -46,18 +52,79 @@ def getAccessToken():
     headers = {
         'X-Forwarded-For': f'{CLIENT_IP}'
     }
-    response = requests.put(AUTHORIZATION, headers=headers, json=credentials, cookies=cookies)
+    response = requests.put(AUTHORIZATION, headers=headers, json=credentials, cookies=COOKIES)
 
     # WHAT IF WRONG USERNAME AND PASSWORD -> FIXING....
 
     uri = response.json()['response']['parameters']['uri']
     access_token = urllib.parse.parse_qs(uri)
     access_token = access_token['https://playvalorant.com/#access_token'][0]
+
     return access_token
+
+
+def getEntitlementToken():
+    ACCESS_TOKEN = getAccessToken()
+    COOKIES = getCookies()
+
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+    }
+    response = requests.post(ENTITLEMENT_TOKEN_LINK, headers=headers, json={}, cookies=COOKIES)
+    entitlement_token = response.json()['entitlements_token']
+
+    res = make_response('adding entitlement_token cookie')
+    res.set_cookie('entitlement_token', entitlement_token)
+
+    return entitlement_token
+
+
+def user():
+    ACCESS_TOKEN = getAccessToken()
+    COOKIES = getCookies()
+
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+        'X-Forwarded-For': f'{CLIENT_IP}'
+    }
+
+    response = requests.post(AUTHORIZATION_INFORMATION, headers=headers, json={}, cookies=COOKIES)
+    data = response.json()
+    user_id = data['sub']
+    riot = data['acct']['game_name']
+    tagline = data['acct']['tag_line']
+    game_name = "{0} #{1}".format(riot, tagline)
+
+    return user_id, game_name
+
+
+def getMatchHistory():
+    PLAYER_ID, IGN = user()
+    ACCESS_TOKEN = getAccessToken()
+    ENTITLEMENT_TOKEN = getEntitlementToken()
+    COOKIES = getCookies()
+    headers = {
+        'Authorization': f'Bearer {ACCESS_TOKEN}',
+        'X-Riot-Entitlements-JWT': f'{ENTITLEMENT_TOKEN}',
+        'X-Riot-ClientPlatform': CLIENT_PLATFORM,
+    }
+
+    MATCH_LINK = f'https://pd.{USER_REGION}.a.pvp.net/mmr/v1/players/{PLAYER_ID}/competitiveupdates?startIndex=0&endIndex=1'
+
+    response = requests.get(MATCH_LINK, headers=headers, cookies=COOKIES)
+
+    data = response.json()['Matches'][0]
+
+    tier_after_update = data['TierAfterUpdate']
+    tier_before_update = data['TierBeforeUpdate']
+    ranked_rating_earned = data['RankedRatingEarned']
+
+    return tier_after_update, tier_before_update, ranked_rating_earned, IGN
 
 
 @app.route('/')
 def start():
+    print(getMatchHistory())
     data = {
 
     }
