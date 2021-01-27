180814b546b00e04f343a866a306e05d19256c49
Sandro Cagara <sandrocagaragithub@gmail.com>
2021-01-27 19:37:36 +0800

    adding getCookies functions

--- api/run-valorant-app.py
+++ api/run-valorant-app.py
@@ -1,9 +1,37 @@
 from flask import Flask, Response, render_template, make_response, request
 from dotenv import load_dotenv, find_dotenv
+import os, socket
+import requests
+
+hostname = socket.gethostname()
 
 load_dotenv(find_dotenv())
 
+CLIENT_IP = socket.gethostbyname(hostname)
+AUTHORIZATION = 'https://auth.riotgames.com/api/v1/authorization'
+
+RIOT_USERNAME = os.getenv("RIOT_USERNAME")
+RIOT_PASSWORD = os.getenv("RIOT_PASSWORD")
+USER_REGION = os.getenv("USER_REGION")
+
 app = Flask(__name__, template_folder="components")
+
+
+def getCookies():
+    credentials = {
+        'client_id': 'play-valorant-web-prod',
+        'nonce': '1',
+        'redirect_uri': 'https://playvalorant.com/',
+        'response_type': 'token id_token',
+        'scope': 'account openid',
+    }
+
+    headers = {
+        'X-Forwarded-For': f'{CLIENT_IP}'
+    }
+    response = requests.post(AUTHORIZATION, headers=headers, json=credentials)
+    cookies = response.cookies
+    return cookies
 
 
 @app.route('/')
